d2d68abe95850d0bf2ec922c5f269e807a0807f0
huchen2000 <huchen2000@gmail.com>
2025-04-03 10:49:54 -0400

    ed - demo

---
+++ addons/fc_demo/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'demo module',
+    'version': '1.0',
+    'category': 'FC/Demo',
+    'description': """This is just a demo""",
+    'depends': [],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'assets': {},
+    'license': 'LGPL-3',
+}
